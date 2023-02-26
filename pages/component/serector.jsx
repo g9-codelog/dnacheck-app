089ff778138ae115d267d826894658be2bcdbe38
G9 <g9.nine.28@gmail.com>
2023-02-26 21:08:20 +0900

    作成の準備を進めました。

---
+++ pages/component/serector.jsx
@@ -0,0 +1,10 @@
+function Serector() {
+    return(
+        <div>
+            <button onClick={console.log("aaa")}>Yes</button>
+            <button>No</button>
+        </div>
+    )
+}
+
+export default Serector;
